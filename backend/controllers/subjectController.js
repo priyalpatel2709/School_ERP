8f3018be15e28b4133a12d068795516bb756f972
priyalpatel2709 <piyu9552@gmail.com>
2024-07-10 14:56:16 +0530

    test

--- backend/controllers/subjectController.js
+++ backend/controllers/subjectController.js
@@ -1,7 +1,5 @@
 const asyncHandler = require("express-async-handler");
-
 const crudOperations = require("../utils/crudOperations");
-
 const {
   getSubjectModel,
   getTeacherModel,
@@ -9,110 +7,66 @@ const {
   getClassModel,
 } = require("../models");
 
-const createSubject = asyncHandler(async (req, res, next) => {
+const getSubjectOperations = (req, populateModels = []) => {
   const Subject = getSubjectModel(req.schoolDb);
+  return crudOperations({ mainModel: Subject, populateModels });
+};
 
-  const subjectOperations = crudOperations({
-    mainModel: Subject,
-    populateModels: [],
-  });
-
-  subjectOperations.create(req, res, next);
+const createSubject = asyncHandler(async (req, res, next) => {
+  getSubjectOperations(req).create(req, res, next);
 });
 
 const getAllSubject = asyncHandler(async (req, res, next) => {
   const Teacher = getTeacherModel(req.schoolDb);
-  const Subject = getSubjectModel(req.schoolDb);
   const Class = getClassModel(req.schoolDb);
   const User = getUserModel(req.usersDb);
 
-  const subjectOperations = crudOperations({
-    mainModel: Subject,
-    populateModels: [
-      {
-        field: "teachers",
-        model: Teacher,
-        select: "user classes subjects",
-        populateFields: [
-          {
-            field: "user",
-            model: User,
-            select: "name",
-          },
-          {
-            field: "classes",
-            model: Class,
-            select: "classNumber division",
-          },
-        ],
-      },
-    ],
-  });
+  const populateModels = [
+    {
+      field: "teachers",
+      model: Teacher,
+      select: "user classes subjects",
+      populateFields: [
+        { field: "user", model: User, select: "name" },
+        { field: "classes", model: Class, select: "classNumber division" },
+      ],
+    },
+  ];
 
-  subjectOperations.getAll(req, res, next);
+  getSubjectOperations(req, populateModels).getAll(req, res, next);
 });
 
 const getSubjectById = asyncHandler(async (req, res, next) => {
   const Teacher = getTeacherModel(req.schoolDb);
-  const Subject = getSubjectModel(req.schoolDb);
   const Class = getClassModel(req.schoolDb);
   const User = getUserModel(req.usersDb);
 
-  const subjectOperations = crudOperations({
-    mainModel: Subject,
-    populateModels: [
-      {
-        field: "teachers",
-        model: Teacher,
-        populateFields: [
-          {
-            field: "user",
-            model: User,
-          },
-          {
-            field: "classes",
-            model: Class,
-          },
-        ],
-      },
-    ],
-  });
+  const populateModels = [
+    {
+      field: "teachers",
+      model: Teacher,
+      populateFields: [
+        { field: "user", model: User },
+        { field: "classes", model: Class },
+      ],
+    },
+  ];
 
-  subjectOperations.getById(req, res, next);
+  getSubjectOperations(req, populateModels).getById(req, res, next);
 });
 
 const updateSubject = asyncHandler(async (req, res, next) => {
-  const Subject = getSubjectModel(req.schoolDb);
-
-  const subjectOperations = crudOperations({
-    mainModel: Subject,
-    populateModels: [],
-  });
-
-  subjectOperations.updateById(req, res, next);
+  getSubjectOperations(req).updateById(req, res, next);
 });
 
 const deleteAllSubject = asyncHandler(async (req, res, next) => {
-  const Subject = getSubjectModel(req.schoolDb);
-
-  const subjectOperations = crudOperations({
-    mainModel: Subject,
-    populateModels: [],
-  });
-
-  subjectOperations.deleteAll(req, res, next);
+  getSubjectOperations(req).deleteAll(req, res, next);
 });
 
 const deleteSubjectById = asyncHandler(async (req, res, next) => {
-  const Subject = getSubjectModel(req.schoolDb);
-
-  const subjectOperations = crudOperations({
-    mainModel: Subject,
-    populateModels: [],
-  });
-
-  subjectOperations.deleteById(req, res, next);
+  getSubjectOperations(req).deleteById(req, res, next);
 });
+
 module.exports = {
   createSubject,
   getAllSubject,
