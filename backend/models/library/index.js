d8d5ebcb87cea88353a288db690ca1d6b0e13e91
priyal027 <priyal.patel@akinolabs.com>
2026-01-21 17:30:52 +0530

    remove folder

--- backend/models/library/index.js
+++
@@ -1,9 +0,0 @@
-const LibraryBook = require('./bookModel');
-const LibraryItem = require('./itemModel');
-const BorrowingRecord = require('./borrowingRecordModel');
-
-module.exports = {
-  LibraryBook,
-  LibraryItem,
-  BorrowingRecord
-}; 
